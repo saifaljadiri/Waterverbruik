58e80e2ac87f14ce22c321c7ebb187ff1551a646
saifaljadiri <39760012+saifaljadiri@users.noreply.github.com>
2018-07-30 20:55:02 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Taxikosten
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Text1 = new System.Windows.Forms.TextBox();
+            this.Text2 = new System.Windows.Forms.TextBox();
+            this.Text3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.lab1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(36, 36);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(772, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // Text1
+            // 
+            this.Text1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Text1.Location = new System.Drawing.Point(241, 154);
+            this.Text1.Multiline = true;
+            this.Text1.Name = "Text1";
+            this.Text1.Size = new System.Drawing.Size(206, 103);
+            this.Text1.TabIndex = 3;
+            this.Text1.TextChanged += new System.EventHandler(this.Text1_TextChanged);
+            // 
+            // Text2
+            // 
+            this.Text2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Text2.Location = new System.Drawing.Point(241, 269);
+            this.Text2.Multiline = true;
+            this.Text2.Name = "Text2";
+            this.Text2.Size = new System.Drawing.Size(206, 103);
+            this.Text2.TabIndex = 4;
+            this.Text2.TextChanged += new System.EventHandler(this.Text2_TextChanged);
+            // 
+            // Text3
+            // 
+            this.Text3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Text3.Location = new System.Drawing.Point(241, 385);
+            this.Text3.Multiline = true;
+            this.Text3.Name = "Text3";
+            this.Text3.Size = new System.Drawing.Size(206, 99);
+            this.Text3.TabIndex = 6;
+            this.Text3.TextChanged += new System.EventHandler(this.Text3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(470, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 99);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lab1
+            // 
+            this.lab1.AutoSize = true;
+            this.lab1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab1.Location = new System.Drawing.Point(482, 80);
+            this.lab1.Name = "lab1";
+            this.lab1.Size = new System.Drawing.Size(75, 40);
+            this.lab1.TabIndex = 16;
+            this.lab1.Text = "Tijd";
+            this.lab1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 40);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Gereden km";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 40);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Dagen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 420);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 40);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Totaal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(57, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 40);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Minuten";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd hh:mm";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(241, 80);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 35);
+            this.dateTimePicker1.TabIndex = 21;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 632);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lab1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Text3);
+            this.Controls.Add(this.Text2);
+            this.Controls.Add(this.Text1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox Text1;
+        private System.Windows.Forms.TextBox Text2;
+        private System.Windows.Forms.TextBox Text3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label lab1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taxikosten
+{
+    public partial class Form1 : Form
+    {
+
+        decimal inputOne;
+        int arth_operator;
+        
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void Text1_TextChanged(object sender, EventArgs e)
+        {
+           // per gereden km is gelijk aan 1 euro (double)
+
+            double d1 = 1.0;
+          
+            
+                    
+            TextBox Text1 = new TextBox();
+        
+
+            if (!string.IsNullOrEmpty(Text1.Text) && !string.IsNullOrEmpty(Text2.Text))
+                Text3.Text = (Convert.ToInt32(Text1.Text) + Convert.ToInt32(Text2.Text)).ToString();
+
+             
+        }
+
+    
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // vrijdag 22:00 tot maandag 7:00  komt toeslag van 15% ( normaal bedrag 8 115%)
+
+        }
+
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //reset button
+
+            Text1.Text = "";
+            Text2.Text = "";
+            Text3.Text = "";
+
+
+            {
+                void ResetAllControls(Control form)
+                {
+                    foreach (Control control in form.Controls)
+                    {
+                        if (control is TextBox);
+                        {
+                            TextBox textBox = (TextBox)control;
+                            textBox.Text = null;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Text2_TextChanged(object sender, EventArgs e)
+        {
+            // per min gereden komt bedrag van 0.25 euro (double) 
+            // per min gereden tussen 8:00 en 18:00 uur komt bedrag van 0.45 euro (double)
+
+            double geredenkm = Convert.ToDouble(Text1.Text);
+            double minuten = Convert.ToDouble(Text2.Text);
+            DayOfWeek dag = dateTimePicker1.Value.DayOfWeek;
+            TimeSpan tijd = dateTimePicker1.Value.TimeOfDay;
+            int uur = tijd.Hours;
+            int min= tijd.Minutes;
+
+            double dagtoeslag = 0.25;
+            double avondtoeslag = 0.45;
+            double total = 0;
+
+            if (uur > 8 && uur < 18)
+            {
+                total = minuten * dagtoeslag;
+            }
+            else
+            {
+                total = minuten * avondtoeslag;
+            }
+
+            total = total + geredenkm;
+
+            bool isWeekend = (  dag.Equals(DayOfWeek.Friday) && uur >= 22) ||
+                                dag.Equals(DayOfWeek.Saturday) ||
+                                (dag.Equals(DayOfWeek.Monday) && uur <= 7);
+            if (isWeekend)
+            {
+                total *= 1.15; // verhoog totaal met 15%
+            }
+
+            
+            Text3.Text = Convert.ToString(total);
+                
+        }
+       
+    }
+}
+
+    
+
